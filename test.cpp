85be3d043f50ecc2f94e6ef88520051e4ec54a29
Grégoire Scano <gregoire.scano@malloc.fr>
2019-01-25 17:57:00 +0800

    More tests

--- test.cpp
+++ test.cpp
@@ -23,3 +23,54 @@ SCENARIO("Opcode", "[opcode]")
   REQUIRE(!is_non_control(opcode::ping));
   REQUIRE(!is_non_control(opcode::pong));
 }
+
+SCENARIO("Frame", "[frame]")
+{
+  REQUIRE(size::max::medium == std::pow(2, 16) - 1);
+  REQUIRE(size::max::large == std::pow(2, 64 - 1) - 1);
+
+  GIVEN("A web socket frame")
+    {
+      frame_t frame;
+
+      WHEN("Toggling values")
+	{
+	  frame.is_final(true);
+	  REQUIRE(frame.control == 0b10000000);
+	  frame.is_final(false);
+	  REQUIRE(frame.control == 0);
+
+	  frame.control = 0b11110000;
+	  frame.has_rsv2(false);
+	  REQUIRE(frame.control == 0b11010000);
+	}
+
+      WHEN("set to 0b10101010")
+	{
+	  frame.control = 0b10101010;
+
+	  THEN("verify")
+	    {
+	      REQUIRE(frame.is_final());
+	      REQUIRE(!frame.has_rsv1());
+	      REQUIRE(frame.has_rsv2());
+	      REQUIRE(!frame.has_rsv3());
+	      REQUIRE(frame.opcode_() == opcode::pong);
+	    }
+	}
+
+      WHEN("set to 0b01010011")
+	{
+	  frame.control = 0b01010011;
+
+	  THEN("verify")
+	    {
+	      REQUIRE(!frame.is_final());
+	      REQUIRE(frame.has_rsv1());
+	      REQUIRE(!frame.has_rsv2());
+	      REQUIRE(frame.has_rsv3());
+	      REQUIRE(!is_valid_opcode(frame.opcode_()));
+	    }
+	}
+    }
+}
